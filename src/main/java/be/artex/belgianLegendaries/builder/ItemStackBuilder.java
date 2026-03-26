780097073e2daf9a48fd94cd4322f6f4c4cddd02
Artex154 <a.stockelynck12@outlook.com>
2026-03-26 21:22:12 +0100

    Added an ItemBuilder

---
+++ src/main/java/be/artex/belgianLegendaries/builder/ItemStackBuilder.java
@@ -0,0 +1,77 @@
+package be.artex.belgianLegendaries.builder;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ItemType;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemStackBuilder {
+    private final ItemStack stack;
+    private Component name = Component.empty();
+    private List<Component> lore = new ArrayList<>();
+
+    private ItemStackBuilder(ItemStack stack) {
+        this.stack = stack;
+    }
+
+    public static ItemStackBuilder create(@NotNull ItemStack stack) {
+        return new ItemStackBuilder(stack);
+    }
+
+    public static ItemStackBuilder create(@NotNull ItemType type) {
+        return create(type.createItemStack());
+    }
+
+    public ItemStackBuilder name(@NotNull Component name) {
+        this.name = name;
+        return this;
+    }
+
+    public ItemStackBuilder name(@NotNull String name) {
+        this.name = Component.text(name);
+        return this;
+    }
+
+    public ItemStackBuilder lore(@NotNull Component... lore) {
+        List<Component> l = new ArrayList<>();
+
+        for (Component s : lore) {
+            l.add(s.decoration(TextDecoration.ITALIC, false));
+        }
+
+        this.lore = l;
+
+        return this;
+    }
+
+    public ItemStackBuilder lore(@NotNull List<Component> lore) {
+        List<Component> l = new ArrayList<>();
+
+        for (Component s : lore) {
+            l.add(s.decoration(TextDecoration.ITALIC, false));
+        }
+
+        this.lore = l;
+        return this;
+    }
+
+    public ItemStack build() {
+        ItemMeta meta = this.stack.getItemMeta();
+
+        if (!this.name.equals(Component.empty()))
+            meta.displayName(this.name.decoration(TextDecoration.ITALIC, false));
+
+        if (!this.lore.isEmpty())
+            meta.lore(lore);
+
+        this.stack.setItemMeta(meta);
+
+        return this.stack;
+    }
+
+}
